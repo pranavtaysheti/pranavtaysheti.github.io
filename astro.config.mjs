9bfe00ef4175030986fc0da0a7174141dd9d5a70
pranavtaysheti <pranavtaysheti@gmail.com>
2023-08-29 20:25:28 +0530

    added integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        assets: true,
-    },
-    site: 'https://pranavtaysheti.github.io',
+  experimental: {
+    assets: true
+  },
+  site: 'https://pranavtaysheti.github.io',
+  integrations: [],
 });
