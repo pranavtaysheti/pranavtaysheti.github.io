fb3ec0fe891f7b045db6bb5d7994ac5c2942fd4e
pranavtaysheti <pranavtaysheti@gmail.com>
2023-09-03 17:07:46 +0530

    updated astro, added mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
   site: 'https://pranavtaysheti.github.io',
-  integrations: [],
+  integrations: [mdx()]
 });
