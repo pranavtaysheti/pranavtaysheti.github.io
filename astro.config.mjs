9987c01829c24cf839b2a879be8cf881dbd11d15
Pranav Taysheti <pranavtaysheti@gmail.com>
2026-05-10 09:10:41 +0000

    remove FG2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig, fontProviders } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
-import preact from "@astrojs/preact";
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {},
   site: 'https://pranavtaysheti.github.io',
-  integrations: [mdx(), preact()],
+  integrations: [mdx(),],
 
   fonts: [
     {
